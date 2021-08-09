2ec0e2b1d3fa4692a06e7de9fe736adc8ee32382
Matheus Carvalho do Carmo <matheuscarvalho@windowslive.com>
2021-08-09 14:25:32 -0300

    add colors

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'config/app_colors.dart';
 import 'modules/movie_details/presentation/movie_details_page.dart';
 
 class App extends StatelessWidget {
@@ -10,6 +11,13 @@ class App extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: TextTheme(
+          subtitle1: TextStyle(color: AppColors.white),
+          caption: TextStyle(color: AppColors.white),
+        ),
+        iconTheme: IconThemeData(
+          color: AppColors.white,
+        ),
       ),
       home: MovieDetailsPage(),
     ).modular();
---
+++ lib/config/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const blackBackground = Color(0xFF0B0B0B);
+  static const blackDivider = Color(0xFF0F0F0F);
+  static const white = Color(0xFFFFFFFF);
+}
